66e8cf77fb7a12dfeb0a9c0c24a9390840afeaed
Garvit Gupta <garvitgupta2606@ce.du.ac.in>
2026-01-19 16:02:14 +0530

    init

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Server is alive!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
