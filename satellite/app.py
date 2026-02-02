03359279a2dfc05d44cf07d51a2269d0e9c1c2b7
Garvit Gupta <garvitgupta2606@ce.du.ac.in>
2026-02-03 03:04:01 +0530

    satellite port update

--- satellite/app.py
+++ satellite/app.py
@@ -268,4 +268,4 @@ def analyze():
 
 if __name__ == '__main__':
     # use_reloader=False is mandatory for GDAL compatibility
-    app.run(host='0.0.0.0', port=8000, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
